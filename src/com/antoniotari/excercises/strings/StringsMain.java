52ccac3c793fa4473f4f247ae894c44f7f892637
antoniotari <antoniotari11@gmail.com>
2015-09-17 17:19:19 -0400

    longestCommonSubsequence

--- src/com/antoniotari/excercises/strings/StringsMain.java
+++ src/com/antoniotari/excercises/strings/StringsMain.java
@@ -60,4 +60,18 @@ public class StringsMain {
 		L.log(count,chars.length);
 		return result;
 	}
+	
+	public static String longestCommonSubsequence(String a, String b){
+	    int aLen = a.length();
+	    int bLen = b.length();
+	    if(aLen == 0 || bLen == 0){
+	        return "";
+	    }else if(a.charAt(aLen-1) == b.charAt(bLen-1)){
+	        return longestCommonSubsequence(a.substring(0,aLen-1),b.substring(0,bLen-1)) + a.charAt(aLen-1);
+	    }else{
+	        String x = longestCommonSubsequence(a, b.substring(0,bLen-1));
+	        String y = longestCommonSubsequence(a.substring(0,aLen-1), b);
+	        return (x.length() > y.length()) ? x : y;
+	    }
+	}
 }
